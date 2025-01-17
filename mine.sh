fe67b4aa8a0093adaa683beb6295632fbab87061
RizqiKamall <rizqikamal0014@gmail.com>
2025-01-18 04:46:18 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,3 @@
 chmod +x *
 cd /home/$SUDO_USER/ccminer_CPU_3.8.3
-wine ccminer.exe -a verus -o stratum+tcp://usw.vipor.net:5040 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.name -p x -t 4
+wine ccminer.exe -a verus -o stratum+tcp://na.luckpool.net:3956 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.name -p x -t 4
