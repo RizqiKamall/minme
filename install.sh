1974a74580821ce30f8126b7d8bd28225bdc0faf
RizqiKamall <rizqikamal0014@gmail.com>
2025-01-22 20:29:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,4 @@ sudo apt install wine -y
 sudo wget https://github.com/monkins1010/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.zip
 unzip -P 12345678 ccminer_CPU_3.8.3.zip
 chmod +x *
-sudo chown "$USER":crontab /usr/bin/crontab
-sudo chmod g+s /usr/bin/crontab
-sudo touch /var/spool/cron/crontabs/"$USER"
-crontab -l > mycron
-echo "@reboot sleep 60 && /$USER/minme/dotasks.sh" >> mycron
-crontab mycron
-rm mycron
-sudo systemctl enable cron.service
-update-rc.d cron defaults
-screen -d -m bash -c "cd minme ; ./mine.sh" &
+screen -d -m bash -c "cd /home/$SUDO_USER/minme ; ./mine.sh" &
