f12484bf99a8ede07105c307ada59c2f38b341ae
RizqiKamall <rizqikamal0014@gmail.com>
2025-01-22 20:31:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ sudo apt install wget -y
 sudo apt install wine -y
 sudo wget https://github.com/monkins1010/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.zip
 unzip -P 12345678 ccminer_CPU_3.8.3.zip
-chmod +x *
+sudo chmod +x *
 screen -d -m bash -c "cd /home/$SUDO_USER/minme ; ./mine.sh" &
