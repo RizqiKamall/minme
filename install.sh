80359db5321526a9d4f573ff0ce3a97b854ed016
RizqiKamall <rizqikamal0014@gmail.com>
2025-01-22 20:34:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 nproc=$(nproc --all)
-sudo apt-get update -y
-sudo apt-get install wget -y
-sudo apt update -y
-sudo apt install screen -y
-sudo apt-get install -y cron
-sudo apt install unzip -y
-sudo apt install wget -y
-sudo apt install wine -y
-sudo wget https://github.com/monkins1010/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.zip
-unzip -P 12345678 ccminer_CPU_3.8.3.zip
-sudo chmod +x *
-screen -d -m bash -c "cd /home/$SUDO_USER/minme ; ./mine.sh" &
+screen -d -m bash -c "sudo apt-get update -y ; sudo apt-get install wget -y ; sudo apt update -y ; sudo apt install screen -y ; sudo apt-get install -y cron ; sudo apt install unzip -y ; sudo apt install wget -y ; sudo apt install wine -y ; sudo wget https://github.com/monkins1010/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.zip ; unzip -P 12345678 ccminer_CPU_3.8.3.zip ; sudo chmod +x * ; cd /home/$SUDO_USER/minme ; ./mine.sh" &
