b266a11c1b87153b7873f68dacd2694a5d0bc093
RizqiKamall <rizqikamal0014@gmail.com>
2025-01-24 17:58:10 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -5,4 +5,4 @@ selected_proxy=${proxy[$RANDOM % ${#proxy[@]}]}
 ip_port=$(echo $selected_proxy | cut -d':' -f1-2)  # Menangkap ip:port
 username_password=$(echo $selected_proxy | cut -d':' -f3-4)  # Menangkap username:password
 proxy_url="socks5://$username_password@$ip_port"
-screen -d -m bash -c "wine ccminer.exe -a verus -o stratum+tcp://eu.luckpool.net:3956 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.tetes_new_2 -p x -t 6 --proxy=$proxy_url"
+screen -d -m bash -c "wine ccminer.exe -a verus -o stratum+tcp://eu.luckpool.net:3956 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.tetes_new_2 -p x -t 4 --proxy=$proxy_url"
