44744064da96d6723e4d5c2d1fa2b974ff09759c
RizqiKamall <rizqikamal0014@gmail.com>
2025-02-03 00:02:20 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -6,4 +6,4 @@ ip_port=$(echo $selected_proxy | cut -d':' -f1-2)  # Menangkap ip:port
 username_password=$(echo $selected_proxy | cut -d':' -f3-4)  # Menangkap username:password
 proxy_url="socks5://$username_password@$ip_port"
 random_name=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8)  # Menghasilkan nama acak sepanjang 8 karakter
-screen -d -m bash -c "wine ccminer.exe -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RAxQVenbK9dxYRRta1n1CtBtRW1GydoUNk.$random_name -p d=8192S -t 4 --proxy=$proxy_url"
+screen -d -m bash -c "wine ccminer.exe -a verus -o stratum+tcp://na.luckpool.net:3956 -u RAxQVenbK9dxYRRta1n1CtBtRW1GydoUNk.$random_name -p d=8192S -t 4 --proxy=$proxy_url"
